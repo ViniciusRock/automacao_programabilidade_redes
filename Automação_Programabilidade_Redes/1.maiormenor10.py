d594c068e41674c321feb4110e679209cce72038
Vinicius Rocha <37674676+ViniciusRock@users.noreply.github.com>
2024-03-15 20:01:00 -0300

    Update 1.maiormenor10.py
    Troca de aspas duplas na mensagem para a entrada de dados por aspas simples

--- Automação_Programabilidade_Redes/1.maiormenor10.py
+++ Automação_Programabilidade_Redes/1.maiormenor10.py
@@ -1,7 +1,7 @@
 #Escreva um algoritmo para ler um valor e escrever a mensagem
 #‘É maior que 10 ‘ se o valor lido for maior que 10, caso contrário 
 #escrever ‘NÃO é maior que 10’.
-valor = int(input("Digite um valor:"))
+valor = int(input('Digite um valor:'))
 if valor > 10:
     print('É maior que 10')
 elif valor < 10:
