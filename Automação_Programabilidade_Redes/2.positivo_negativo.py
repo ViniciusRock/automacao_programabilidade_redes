22ebb6f61b0353f4cae2dca66e26c2a0af7753d5
Vinicius Rocha <37674676+ViniciusRock@users.noreply.github.com>
2024-03-15 20:08:21 -0300

    Exercício 2

---
+++ Automação_Programabilidade_Redes/2.positivo_negativo.py
@@ -0,0 +1,9 @@
+#Escreva um algoritmo para ler um valor e escrever se é positivo
+#ou negativo. Considere o valor zero como positivo.
+valor = int(input('Informe um número:'))
+
+#Considerando que 0 é positivo
+if valor >= 0:
+    print('Positivo')
+else:
+    print('Negativo')
